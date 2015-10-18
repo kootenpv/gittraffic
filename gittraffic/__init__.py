e32c2a01f068c99d8021efd0dc217f9886734ffa
Pascal van Kooten <kootenpv@gmail.com>
2015-10-18 15:37:44 +0200

    deploy issue

--- gittraffic/__init__.py
+++ gittraffic/__init__.py
@@ -1,2 +1,2 @@
 __project__ = 'gittraffic'
-__version__ = "0.0.2"
+__version__ = "0.0.4"
--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ from setuptools import setup
 
 MAJOR_VERSION = '0'
 MINOR_VERSION = '0'
-MICRO_VERSION = '2'
+MICRO_VERSION = '4'
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
-setup(name = 'gittraffic',
-      version = VERSION,
-      description = 'Follow your git traffic per package in a github repo',
-      url = 'https://github.com/kootenpv/gittraffic',
-      author = 'Pascal van Kooten',
-      author_email = 'kootenpv@gmail.com',
-      license = 'GPL',
-      packages = ['gittraffic'],
-      install_requires = [ 
+setup(name='gittraffic',
+      version=VERSION,
+      description='Follow your git traffic per package in a github repo',
+      url='https://github.com/kootenpv/gittraffic',
+      author='Pascal van Kooten',
+      author_email='kootenpv@gmail.com',
+      license='GPL',
+      packages=['gittraffic'],
+      install_requires=[
           'lxml', 'datetime', 'selenium'
-      ], 
-      entry_points = { 
-          'console_scripts': ['gittraffic = gittraffic.gittraffic:main'] 
+      ],
+      entry_points={
+          'console_scripts': ['gittraffic = gittraffic.gittraffic:main']
       },
-      classifiers = [ 
+      classifiers=[
           'Environment :: Console',
           'Intended Audience :: Developers',
           'Intended Audience :: Customer Service',
@@ -28,18 +28,18 @@ setup(name = 'gittraffic',
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: Unix',
           'Operating System :: POSIX',
-          'Programming Language :: Python', 
+          'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3', 
+          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
-          'Topic :: Software Development', 
+          'Topic :: Software Development',
           'Topic :: Software Development :: Build Tools',
-          'Topic :: Software Development :: Debuggers', 
+          'Topic :: Software Development :: Debuggers',
           'Topic :: Software Development :: Libraries',
           'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: System :: Software Distribution', 
+          'Topic :: System :: Software Distribution',
           'Topic :: Utilities'
-    ], 
-    zip_safe = False,
-    platforms='any')
+      ],
+      zip_safe=False,
+      platforms='any')
