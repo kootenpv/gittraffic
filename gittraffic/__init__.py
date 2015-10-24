b71fbe120a240da5a67acf2c690ed1d86cc237ed
Pascal van Kooten <kootenpv@gmail.com>
2015-10-24 09:54:54 +0200

    updated plotting directly from python gitplot.py

--- gittraffic/__init__.py
+++ gittraffic/__init__.py
@@ -1,2 +1,2 @@
 __project__ = 'gittraffic'
-__version__ = "0.0.4"
+__version__ = "0.0.5"
--- gittraffic/gitplot.py
+++ gittraffic/gitplot.py
@@ -1,6 +1,15 @@
 from collections import Counter
 import json
+
+# %pylab
+import numpy
 import matplotlib
+from matplotlib import pylab, mlab, pyplot
+np = numpy
+plt = pyplot
+from IPython.core.pylabtools import figsize, getfigs
+from pylab import *
+from numpy import *
 
 
 def plot_package(js, key):
@@ -9,8 +18,8 @@ def plot_package(js, key):
         if x[0][:10] not in totals:
             totals[x[0][:10]] = 0
         totals[x[0][:10]] += int(x[-1])
-    matplotlib.pyplot.plot([totals[x] for x in sorted(totals)], label=key)
-    matplotlib.pyplot.legend(loc='best')
+    plt.plot([totals[x] for x in sorted(totals)], label=key)
+    plt.legend(loc='best')
 
 
 def plot_all(gittraffic_path='gittraffic.json'):
@@ -29,3 +38,8 @@ def most_common_source(js, key):
         totals[x[1]] += int(x[-1])
     # has to still be divided by amount of unique days
     print(totals.most_common())
+
+if __name__ == '__main__':
+    plot_all()
+    plt.show()
+    input('Waiting to close plot...')
